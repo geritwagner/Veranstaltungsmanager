3c0470b89965d9501ae801885bf1faeb977adf7d
Gerit <gerit1989@googlemail.com>
2014-01-28 11:27:50 +0100

    Datumsformat im Modul (Newstelegramm) aktualisiert, Anzeige der Links angepasst (Ausschreibung etc. nur bis zum Veranstaltungsdatum)

--- site/module/tmpl/default.php
+++ site/module/tmpl/default.php
@@ -19,23 +19,27 @@ if(count((array)$events)) {
 		echo '<p>';
 		echo $event->Ort;
 		echo ', ';
-		echo $event->Datum_von->calendar('d.m.y');
 		if($event->Datum_von->toUnix() != $event->Datum_bis->toUnix()) {
-			echo ' - ';
+			echo $event->Datum_von->calendar('d.');	
+			echo '-';
 			echo $event->Datum_bis->calendar('d.m.y');
+		} else {
+			echo $event->Datum_von->calendar('d.m.y');			
 		}
 		echo '<br />';
 		echo '<strong>'.$event->Bezeichnung.'</strong>';
 		
 		foreach($event->links as $link) {
+			if(!( ($link->Typ=="Ausschreibung" || $link->Typ=="Zeitplan" ||$link->Typ=="Teilnehmer" ) && $event->Datum_bis < new Jdate())){
 			echo '<br />';
 			echo '<a target="_blank" href="'.$link->URL.'">'.$link->Bezeichnung.'</a>';
+			}
 		}
 		
 		echo '</p>';
 		echo '<hr style="margin: 2px 0; border: 0; border-bottom: 1px solid #C5C5C5;" />';
 	}
-	echo '<p style="text-align: center; margin-top: 10px; font-weight: bold">Weitere Ergebnisse in der&nbsp; <a href="veranstaltungen">Ergebnisrubrik</a>...</p>';
+	echo '<p style="text-align: center; margin-top: 10px; font-weight: bold">Weitere Ergebnisse in der&nbsp; <a href="index.php?option=com_events&view=events&jahr='.date("Y").'&noheadline=1">Ergebnisrubrik</a>...</p>';
 } else {
 	echo '<p>Es sind noch keine Einträge vorhanden.</p>';
 }
