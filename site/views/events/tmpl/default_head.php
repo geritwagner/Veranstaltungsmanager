d1580154e9c8aa3c44cbe3dad37f9495f914c8ae
Gerit <gerit1989@googlemail.com>
2014-02-09 16:21:00 +0100

    Links zu den Monaten in den Views "Veranstaltungen" und "Ergebnisse" aktualisiert

--- site/views/events/tmpl/default_head.php
+++ site/views/events/tmpl/default_head.php
@@ -13,18 +13,18 @@ defined('_JEXEC') or die('Restricted Access');
 ?>
 <table width="100%" border="0">
 	<tr>
-		<td><a href="#Januar" mce_href="#Januar">Jan</a></td>
-		<td><a href="#Februar" mce_href="#Februar">Feb</a></td>
-		<td><a href="#März" mce_href="#März">März</a></td>
-		<td><a href="#April" mce_href="#April">Apr</a></td>
-		<td><a href="#Mai" mce_href="#Mai">Mai</a></td>
-		<td><a href="#Juni" mce_href="#Juni">Jun</a></td>
-		<td><a href="#Juli" mce_href="#Juli">Jul</a></td>
-		<td><a href="#August" mce_href="#August">Aug</a></td>
-		<td><a href="#September" mce_href="#September">Sept</a></td>
-		<td><a href="#Oktober" mce_href="#Oktober">Okt</a></td>
-		<td><a href="#November" mce_href="#November">Nov</a></td>
-		<td><a href="#Dezember" mce_href="#Dezember">Dez</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#Januar" mce_href="#Januar">Jan</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#Februar" mce_href="#Februar">Feb</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#März" mce_href="#März">März</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#April" mce_href="#April">Apr</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#Mai" mce_href="#Mai">Mai</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#Juni" mce_href="#Juni">Jun</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#Juli" mce_href="#Juli">Jul</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#August" mce_href="#August">Aug</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#September" mce_href="#September">Sept</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#Oktober" mce_href="#Oktober">Okt</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#November" mce_href="#November">Nov</a></td>
+		<td><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#Dezember" mce_href="#Dezember">Dez</a></td>
 	</tr>
 </table>
 <p>&nbsp;</p>
